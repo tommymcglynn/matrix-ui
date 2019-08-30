ce7161538628a854a1d2255bdad0fbb0c41f5171
Tommy McGlynn <tmcglynn@apple.com>
2019-08-30 12:28:17 -0700

    Many refinements

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Matrix from "./Matrix";
 
+const axios = require('axios');
+
 const MIN_SIZE = 2;
-const MAX_SIZE = 10;
+const MAX_SIZE = 20;
 const DEFAULT_SIZE = 5;
 const MAX_CELL_SIZE = 60;
 
@@ -11,11 +13,14 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {cols: DEFAULT_SIZE, rows: DEFAULT_SIZE, cellSize: MAX_CELL_SIZE, showSettings: false};
+        this.state = {cols: DEFAULT_SIZE, rows: DEFAULT_SIZE, cellSize: MAX_CELL_SIZE, showSettings: false, statusMessage: "", matrixData: this.constructData(DEFAULT_SIZE, DEFAULT_SIZE, null)};
 
         this.onColCountChange = this.onColCountChange.bind(this);
         this.onRowCountChange = this.onRowCountChange.bind(this);
         this.onWindowResize = this.onWindowResize.bind(this);
+        this.onMatrixDataChange = this.onMatrixDataChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onClear = this.onClear.bind(this);
     }
 
     componentDidMount() {
@@ -27,10 +32,27 @@ class App extends React.Component {
         window.removeEventListener('resize', this.onWindowResize);
     }
 
+    constructData(colCount, rowCount, data) {
+        let newData = [];
+        for (let row = 0; row < rowCount; row++) {
+            let dataCols = [];
+            for (let col = 0; col < colCount; col++) {
+                let value = data != null && data[row] != null && data[row][col] ? data[row][col] : 0;
+                dataCols.push(value);
+            }
+            newData.push(dataCols);
+        }
+
+        return newData;
+    }
+
+    calculateCellSize() {
+        let maxWidth = window.innerWidth * 0.6;
+        return Math.min(Math.floor(maxWidth / this.state.cols / 10) * 10, MAX_CELL_SIZE);
+    }
+
     onWindowResize() {
-        let maxWidth = window.innerWidth * 0.8;
-        let cellSize = Math.min(Math.floor(maxWidth / this.state.cols / 10) * 10, MAX_CELL_SIZE);
-        this.setState({ cellSize: cellSize });
+        this.setState({ cellSize: this.calculateCellSize() });
     }
 
     onColCountChange(e) {
@@ -38,7 +60,12 @@ class App extends React.Component {
         cols = (cols === undefined) ? MIN_SIZE : parseInt(cols);
         cols = isNaN(cols) ? MIN_SIZE : cols;
         cols = cols > MAX_SIZE ? MAX_SIZE : cols;
-        this.setState({cols: cols})
+        this.setState({
+            cols: cols,
+            matrixData: this.constructData(cols, this.state.rows, this.state.matrixData),
+            statusMessage: "",
+            cellSize: this.calculateCellSize()
+        });
     }
 
     onRowCountChange(e) {
@@ -46,7 +73,37 @@ class App extends React.Component {
         rows = (rows === undefined) ? MIN_SIZE : parseInt(rows);
         rows = isNaN(rows) ? MIN_SIZE : rows;
         rows = rows > MAX_SIZE ? MAX_SIZE : rows;
-        this.setState({rows: rows})
+        this.setState({
+            rows: rows,
+            matrixData: this.constructData(this.state.cols, rows, this.state.matrixData),
+            statusMessage: "",
+            cellSize: this.calculateCellSize()
+        });
+    }
+
+    onMatrixDataChange(data) {
+        this.setState({matrixData: data});
+    }
+
+    onSubmit() {
+        let data = this.constructData(this.state.cols, this.state.rows, this.state.matrixData);
+        let self = this;
+        axios.post('/matrix', {
+            data: data
+        })
+        .then(function (response) {
+            self.setState({statusMessage: response.data.message});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    onClear() {
+        this.setState({
+            matrixData: this.constructData(this.state.cols, this.state.rows, null),
+            statusMessage: ""
+        });
     }
 
     render() {
@@ -77,10 +134,13 @@ class App extends React.Component {
                 {(this.state.showSettings ? settings : null)}
                 {(this.state.showSettings ? overlay : null)}
                 <div className="mainContainer">
-                    <Matrix cols={this.state.cols} rows={this.state.rows} cellSize={this.state.cellSize} />
-                    <div className="submitButton">Submit</div>
+                    <Matrix cols={this.state.cols} rows={this.state.rows} cellSize={this.state.cellSize} data={this.state.matrixData} onChange={this.onMatrixDataChange} />
+                    <div>
+                        <div className="button clear" onClick={() => this.onClear()}>Clear</div>
+                        <div className="button submit" onClick={() => this.onSubmit()}>Submit</div>
+                    </div>
                     <div className="statusMessage">
-                        {this.state.cols}x{this.state.rows} Status message
+                        {this.state.cols}x{this.state.rows} {this.state.statusMessage}
                     </div>
                 </div>
             </div>
--- src/Matrix.js
+++ src/Matrix.js
@@ -2,37 +2,20 @@ import React from 'react';
 
 import './Matrix.css';
 
-const MAX_DATA_SIZE = 20;
-
 class Matrix extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {data: Matrix.constructData(MAX_DATA_SIZE, MAX_DATA_SIZE)};
-
         this.onClickCell = this.onClickCell.bind(this);
     }
 
-    static constructData(colCount, rowCount) {
-        let newData = [];
-        for (let row = 0; row < rowCount; row++) {
-            let dataCols = [];
-            for (let col = 0; col < colCount; col++) {
-                dataCols.push(0);
-            }
-            newData.push(dataCols);
-        }
-
-        return newData;
-    }
-
     onClickCell(col, row) {
-        let data = this.state.data;
+        let data = this.props.data;
         let value = data[row][col];
         value = value !== undefined ? value === 1 : false;
         data[row][col] = value ? 0 : 1;
-        this.setState({data: data});
+        this.props.onChange(data);
     }
 
     render() {
@@ -40,8 +23,7 @@ class Matrix extends React.Component {
         let colCount = this.props.cols;
         let rowCount = this.props.rows;
         let cellSize = this.props.cellSize;
-
-        let data = this.state.data;
+        let data = this.props.data;
 
         let cellIndex = 0;
         let onClickCell = this.onClickCell;
